7a67369c1d74dab7739eb383987fa12a9fd235a8
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2023-05-02 18:11:19 -0400

    update sha256 hash

--- Formula/ouctl.rb
+++ Formula/ouctl.rb
@@ -2,7 +2,7 @@ class Ouctl < Formula
   desc "Deploys openunison into kubernetes"
   homepage "https://openunison.github.io/deployauth/"
   url "https://github.com/TremoloSecurity/openunison-control/archive/refs/tags/v0.0.11.tar.gz"
-  sha256 "56855096f60e39561c734f77367f7b9742fc3faa99e7562b4411062984c7eb5c"
+  sha256 "b1a0428e4f7a72ad9e4818333a8481c148ed698ec619be6c1e4e9441d48eb90c"
   license "Apache-2.0"
 
   depends_on "go" => :build
