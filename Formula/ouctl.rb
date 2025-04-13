de2f38c3f921697a3095d9236d4f95e585bdb219
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2025-04-13 11:58:22 -0400

    Update ouctl.rb
    integrate v0.0.13

--- Formula/ouctl.rb
+++ Formula/ouctl.rb
@@ -1,8 +1,8 @@
 class Ouctl < Formula
   desc "Deploys openunison into kubernetes"
   homepage "https://openunison.github.io/deployauth/"
-  url "https://github.com/TremoloSecurity/openunison-control/archive/refs/tags/v0.0.12.tar.gz"
-  sha256 "7b008d7276999884ae1f89658c72a949598ae4b8bcf2242d13c015b3e0c3450a"
+  url "https://github.com/TremoloSecurity/openunison-control/archive/refs/tags/v0.0.13.tar.gz"
+  sha256 "7a4c59194c97c1a1d4a2b2a076ff8f877b264a5a10a034e1764afb7af9bfa947"
   license "Apache-2.0"
 
   depends_on "go" => :build
